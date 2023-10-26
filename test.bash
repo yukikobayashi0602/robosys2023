b4a165621180c76be75f969b9f2dd5862b1f0232
yukikobayashi0602 <Yu.glanz1@au.com>
2023-10-26 13:23:46 +0900

    improve

--- test.bash
+++ test.bash
@@ -14,6 +14,11 @@ res=0
 out=$(seq 5 | ./plus)
 
 [ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]       || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 [ "$res" = 0 ]  && echo OK
 exit $res
 
