82400b9762a7b8fafd5b59f10cca213c37995a5b
yukikobayashi0602 <Yu.glanz1@au.com>
2023-10-26 13:26:08 +0900

    improve

--- test.bash
+++ test.bash
@@ -19,6 +19,10 @@ out=$(seq 5 | ./plus)
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]       || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO} 
+[ "${out}" = "" ] || ng ${LINENO}
 [ "$res" = 0 ]  && echo OK
 exit $res
 
